f082abd998b15f00aaaa316b52ed3ce60d52f6bd
Anders <Anders@Anderss-MacBook-Pro.local>
2024-06-21 14:49:05 -0400

    wip

--- components/AppMetadata.jsx
+++ components/AppMetadata.jsx
@@ -23,27 +23,5 @@ export const AppMetadata = {
 	],
 	creator: author,
 	authors: [{ name: author, url: url }],
-	colorScheme: "dark",
-	openGraph: {
-		title: `${author} | Portfolio`,
-		description: description,
-		url: url,
-		siteName: `${author} | Portfolio`,
-		images: [
-			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
-				width: 800,
-				height: 600,
-				alt: "My personal portfolio website"
-			},
-			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
-				width: 1800,
-				height: 1600,
-				alt: "My personal portfolio website"
-			}
-		],
-		locale: "en-US",
-		type: "website"
-	}
+	colorScheme: "dark"
 };
